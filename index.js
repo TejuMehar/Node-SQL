467a9044c60c609c06239f5188aac2c9ca74c33e
Tejas mehar <tejas mehar7@gmail.com>
2025-10-08 09:28:14 +0530

    pass Users Array using placeholder

--- index.js
+++ index.js
@@ -12,11 +12,17 @@ const connection = mysql.createConnection({
 
 const schema = fs.readFileSync('schema.sql', 'utf-8');
 
-let q = "INSERT INTO users (username, email, password) VALUES (?,?,?,?)";
-let user = ["123","123_newuser","123@gmail.com","123"]
+//Inserting a New Data
+let q = "INSERT INTO users (username, email, password) VALUES ?";
+let users = [
+  ["123_newuserb","123@gmail.comb","123b"],
+  ["123_newuserc","123@gmail.comc","123c"],
+  ["123_newuserd","123@gmail.comd","123d"]
+
+];
 
 try{
- connection.query(q,user,(err, results) => {
+ connection.query(q,[users],(err, results) => {
   if (err) {
     console.error("Error executing query:", err);
     return;
